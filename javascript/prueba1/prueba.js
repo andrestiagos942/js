b2d2e16b91d07fb21d5cceff98435c9f224ec050
unknown <andrestiagos942@gmail.com>
2021-09-25 19:33:26 -0300

    ultimo js basico

--- javascript/prueba1/prueba.js
+++ javascript/prueba1/prueba.js
@@ -1,5 +1,15 @@
-alert("bienvenido a java script")
-var nombre = prompt("tuseñora tu señora")
-var texto = "hola " + nombre + "me alegro de conocerte"
-alert(texto)
-console.log(texto)
+//var objeto = {clave1: "valor1", clave2: 1234, clave3: "valor3", clave4: 321}
+
+//for (clave in objeto){
+//console.log(clave);
+//console.log(objeto[clave])
+//}
+var objeto2 = {
+color: "rojo",
+numero: 35,
+metodo: function(){
+alert("esta es la funcion del objeto");
+}
+}
+
+objeto2.metodo()
